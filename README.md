8c2f446cd57d71f884f2d2de8b88e9d0aac20707
Sonal Maniya <sonal@atliq.com>
2021-01-31 14:28:33 +0530

    #Added  - npm package version 1.0.0

---
+++ README.md
@@ -0,0 +1,78 @@
+# @atliq/react-native-starter
+
+This project is a [React Native](https://facebook.github.io/react-native/) boilerplate that can be used to kickstart a mobile application.
+
+The boilerplate provides **an optimized architecture for building solid cross-platform mobile applications** through separation of concerns between the UI and business logic. It contains redux, saga, context, theme, localization, tabs and stack navigation.
+
+#### Quick start
+
+```
+$ npx @atliq/react-native-starter <ProjectName>
+
+$ cd <ProjectName>
+
+$ npx react-native run-ios
+
+$ npx react-native run-android
+```
+
+## Directory Structure
+
+```
+root
+├── __tests__
+├── android
+├── ios
+└── App
+    └── Actions
+    |   ├── Keys
+    └── ApiConfig
+    └── AppContext
+    └── Localization
+    └── Reducers
+    |   ├──Default
+    └── Routes
+    └── Sagas
+    └── Screens
+    |    CommonComponent
+    |    Components
+    |    SubComponents
+    └── Services
+    └── Stores
+    └── Theme
+    |    Images
+    └── Utils
+├── fastlane
+├── .env
+...
+```
+
+## Preconfigured with
+
+- Latest react native version
+
+- Redux saga with persistReducer
+
+- Localization
+
+- Theme support (Dark / Light)
+
+- Utility for validations and error messages
+
+- Custom font and font size for maintain typography
+
+- .env setup
+
+- Support different env for PRODUCTION and DEVELOPMENT
+
+- User Authemntication flow
+
+- UI for Login, Tabs and Settings
+
+- Fastlane setup
+
+<br/>
+
+## License
+
+This project is released under the (IST).
---
+++ index.js
@@ -0,0 +1,65 @@
+#! /usr/bin/env node
+const { spawn } = require("child_process");
+const fs = require("fs");
+function runCommand(command, args, options = undefined) {
+  const spawned = spawn(command, args, options);
+
+  return new Promise((resolve) => {
+    spawned.stdout.on("data", (data) => {
+      console.log(data.toString());
+    });
+
+    spawned.stderr.on("data", (data) => {
+      console.error(data.toString());
+    });
+
+    spawned.on("close", () => {
+      resolve();
+    });
+  });
+}
+
+const main = async (repositoryUrl) => {
+  //Get the name of the app-directory to make
+  const directoryName = process.argv[2];
+  if (!directoryName || directoryName.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
+    return console.log(`
+        Invalid directory name.
+        Usage : '@atliq/react-native-starter name_of_app'
+        `);
+  }
+  //1. Clone the repository into the given name
+  await runCommand("git", ["clone", repositoryUrl, "reactStarter"]);
+
+  //2. get the json from package.json
+  const packageJsonRaw = fs.readFileSync(`reactStarter/package.json`);
+  const packageJson = JSON.parse(packageJsonRaw);
+  const dependencyList = Object.keys(packageJson.dependencies);
+
+  console.log("Now, installing react-native...");
+  await runCommand("npx", ["react-native", "init", directoryName]);
+
+  //3. Installing the dependencies.
+  console.log("installing... ", dependencyList);
+  await runCommand("yarn", ["add", ...dependencyList], {
+    cwd: `${process.cwd()}/${directoryName}`,
+  });
+
+  await runCommand("mv", [`reactStarter/App`, `${directoryName}`]);
+  await runCommand("mv", [`reactStarter/.env`, `${directoryName}`]);
+  await runCommand("mv", [`reactStarter/fastlane`, `${directoryName}`]);
+
+  await runCommand("rm", ["-rf", `${directoryName}/App.js`]);
+  await runCommand("npx", ["pod-install"], {
+    cwd: `${process.cwd()}/${directoryName}`,
+  });
+  await runCommand("rm", ["-rf", `reactStarter`]);
+
+  console.log(`Application generated is ready to use.
+To get started, 
+- cd ${directoryName}
+- npm run dev
+  `);
+};
+
+return main("https://github.com/sonalmaniya/react-native-boots.git");
